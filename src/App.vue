aea5e61590901bbd7e4fad93e740a3aa96133c4f
chuangfe <chuangfe@gmail.com>
2021-10-23 20:45:52 +0800

    Merge branch 'NavBar'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+    <transition name="mask">
+      <div
+        class="mask-local"
+        v-show="isMenu"
+        @click="isMenu = isFocus = false"
+      ></div>
+    </transition>
+
+    <NavBar
+      :isMenu="isMenu"
+      :isFocus="isFocus"
+      @setMenu="setMenuHandler"
+      @setFocus="setFocusHandler"
+    />
+
     <main class="main">
       <SectionCamera
         :windowCenter="windowCenter"
@@ -12,12 +27,16 @@
 
 <script>
 import SectionCamera from "./components/SectionCamera/index";
+import NavBar from "./components/NavBar/index";
 
 export default {
   name: "App",
 
   data() {
     return {
+      isMenu: false,
+      isFocus: false,
+
       // 等所有 el 樣式固定後, 再次修改值.
       isLoaded: 0,
 
@@ -29,6 +48,17 @@ export default {
   },
 
   methods: {
+    setMenuHandler(val) {
+      this.isMenu = val;
+    },
+    setFocusHandler(val) {
+      this.isFocus = val;
+    },
+
+    windowScrollHandler(e) {
+      if (e.currentTarget !== window) return false;
+    },
+
     windowScrollHandler(e) {
       if (e.currentTarget !== window) return false;
 
@@ -41,6 +71,7 @@ export default {
 
   components: {
     SectionCamera,
+    NavBar,
   },
 
   mounted() {
@@ -62,4 +93,49 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+/**
+* z-index 順序.
+* NavBar, 1000.
+* NavLocal, 100.
+* mask-local, 10.
+*/
+
+@import "./assets/style/variable.scss";
+@import "./assets/style/class.scss";
+
+#app {
+  padding-bottom: 5000px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  // 因為 NavBar NavLocal .mask-local 需要 body 的 height: 100%; 所以不能讓 #app 有 position: relative;
+  .mask-local {
+    width: 100%;
+    height: 100%;
+    background-color: rgba($black-000000, 0.4);
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    z-index: 10;
+  }
+}
+
+// 進場前的樣式.
+.mask-enter,
+// 離場結束的樣式.
+.mask-leave-to {
+  opacity: 0;
+}
+// 進場結束的樣式.
+.mask-enter-to,
+// 離場前的樣式.
+.mask-leave {
+  opacity: 1;
+}
+// 進場動畫.
+.mask-enter-active,
+// 離場動畫.
+.mask-leave-active {
+  transition: opacity 0.6s ease 0s;
+}
+</style>
---
+++ src/components/NavBar/index.vue
@@ -0,0 +1,48 @@
+<script>
+let listContainer;
+
+export default {
+  name: "NavBar",
+
+  props: {
+    isMenu: {
+      type: Boolean,
+      required: true,
+    },
+
+    isFocus: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  methods: {
+    setMenuHandler() {
+      this.$emit("setMenu", !this.isMenu);
+      listContainer.classList.remove("overflow-auto");
+    },
+
+    positionContainerTransitionendHandler(e) {
+      if (e.propertyName === "height") {
+        listContainer.classList.add("overflow-auto");
+      }
+    },
+
+    searchFocusHandler() {
+      listContainer.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+      this.$emit("setFocus", true);
+    },
+  },
+
+  mounted() {
+    listContainer = this.$el.querySelector(".mobile-container .list-container");
+  },
+};
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
